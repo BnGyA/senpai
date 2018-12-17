d81c40e6f351e3cd2b556501e6f0b06fe0a4eb12
Benjamin Rochez <rochez.benjamin@gmail.com>
2018-12-17 16:28:16 +0100

    Added axios

--- vuejs/vue_cli/src/Blogs.vue
+++ vuejs/vue_cli/src/Blogs.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="blogs">
         <h2>{{ blogTitle }}</h2>
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{post.title}}</h3>
+            <p>{{post.body}}</p>
+        </div>
+
         <button @click="changeTitle">Change Title</button>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Blogs',
     data() {
         return {
-            blogTitle: 'Blogs'
+            blogTitle: 'Blogs',
+            posts: []
         }
     },
     methods: {
@@ -18,14 +26,13 @@ export default {
             this.blogTitle = 'Amazing Blog Site'
         }
     },
-    beforeCreate(){
-        alert('beforeCreate hook')
-    },
     created(){
-        alert('created hook')
-    },
-    beforeUpdate(){
-        alert('beforeUpdate hook')
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then(response =>{
+            this.posts = response.data
+        }).catch(err =>{
+            console.log(err)
+        })
     }
 }
 </script>
