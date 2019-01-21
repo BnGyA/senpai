d515f39108d18f96ae1b9058bf04b15093954e40
Benjamin Rochez <rochez.benjamin@gmail.com>
2019-01-21 14:41:56 +0100

    Added vuetify navigation

---
+++ vuejs/vuetify/todo/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon >
+                <span class="font-weight-light">Test</span>
+                <span>Yolo</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app temporary class="indigo">
+            <p>hi</p>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            drawer: false
+        }
+    }
+}
+</script>
--- vuejs/vuetify/todo/src/views/Home.vue
+++ vuejs/vuetify/todo/src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
-  <HelloWorld />
+<div>
+  <h1>This is the home page</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, illum neque? Veniam laudantium architecto eaque, mollitia obcaecati autem sint fugit impedit sunt et exercitationem reiciendis necessitatibus velit illum dolorum deserunt?</p>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  
 
   export default {
-    components: {
-      HelloWorld
-    }
+    
   }
 </script>
