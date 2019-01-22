4d958cfa6ae5a2f8ca08300748216a0f9af8f65c
Benjamin Rochez <rochez.benjamin@gmail.com>
2019-01-22 10:44:52 +0100

    Grid system

--- vuejs/vuetify/todo/src/views/Dashboard.vue
+++ vuejs/vuetify/todo/src/views/Dashboard.vue
@@ -2,7 +2,33 @@
 <div>
   <h1 class="subheading grey--text">Dashboard</h1>
   <v-container class="my-5">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore quos velit pariatur sit dignissimos est nostrum debitis, quas alias ipsam maiores fuga voluptatibus nobis excepturi aliquid obcaecati recusandae suscipit.</p>
+        <v-layout row>
+          <v-flex xs12 md6>
+            <v-btn outline block class="info">1</v-btn>
+          </v-flex>
+          <v-flex xs12 md6>
+            <v-btn outline block class="info">2</v-btn>
+          </v-flex>
+        </v-layout>
+
+
+        <v-layout row wrap justify-center>
+          <v-flex md4>
+            <v-btn outline block class="info">2</v-btn>
+          </v-flex>
+          <v-flex md4>
+            <v-btn outline block class="info">2</v-btn>
+          </v-flex>
+        </v-layout>
+
+         <v-layout row wrap justify-space-between>
+          <v-flex md3>
+            <v-btn outline block class="info">1</v-btn>
+          </v-flex>
+          <v-flex md3>
+            <v-btn outline block class="info">2</v-btn>
+          </v-flex>
+        </v-layout>
     </v-container>
   </div>
 </template>
