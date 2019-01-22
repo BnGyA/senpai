5494f66565165459b200d8761e91fd1c7a8e3a64
Benjamin Rochez <rochez.benjamin@gmail.com>
2019-01-22 11:56:33 +0100

    Layout dashboard kdm

--- vuejs/vuetify/todo/src/views/Dashboard.vue
+++ vuejs/vuetify/todo/src/views/Dashboard.vue
@@ -3,31 +3,65 @@
   <h1 class="subheading grey--text">Dashboard</h1>
   <v-container class="my-5">
         <v-layout row>
-          <v-flex xs12 md6>
-            <v-btn outline block class="info">1</v-btn>
-          </v-flex>
-          <v-flex xs12 md6>
-            <v-btn outline block class="info">2</v-btn>
-          </v-flex>
-        </v-layout>
+          <v-flex md4 pa-1>
+            <v-card>
+              <v-img src="https://dummyimage.com/300x200/000/fff" height="200px"></v-img>
+              <v-card-title primary-title>Card 1</v-card-title>
+              <v-card-text>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae minus, enim, ipsam tempora ipsa dignissimos quo eligendi eaque fuga voluptate maxime necessitatibus, soluta vitae. Eligendi voluptas ullam vitae cupiditate reiciendis? 
+              </v-card-text>
+              <v-card-actions>
+                <v-layout row  align-end justify-end>                
+                  
+                  <v-btn color="success">Sucess</v-btn>
+                  <v-btn color="error">Error</v-btn>  
+                
+                </v-layout>
 
+              </v-card-actions>
+              
+            </v-card>
+          </v-flex>
+          <v-flex md4 pa-1>
+            <v-card>
+              <v-img src="https://dummyimage.com/300x200/000/fff" height="200px"></v-img>
+              <v-card-title primary-title>Card 1</v-card-title>
+              <v-card-text>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae minus, enim, ipsam tempora ipsa dignissimos quo eligendi eaque fuga voluptate maxime necessitatibus, soluta vitae. Eligendi voluptas ullam vitae cupiditate reiciendis? 
+              </v-card-text>
+              <v-card-actions>
+                <v-layout row  align-end justify-end>                
+                  
+                  <v-btn color="success">Sucess</v-btn>
+                  <v-btn color="error">Error</v-btn>  
+                
+                </v-layout>
 
-        <v-layout row wrap justify-center>
-          <v-flex md4>
-            <v-btn outline block class="info">2</v-btn>
+              </v-card-actions>
+              
+            </v-card>
           </v-flex>
-          <v-flex md4>
-            <v-btn outline block class="info">2</v-btn>
-          </v-flex>
-        </v-layout>
 
-         <v-layout row wrap justify-space-between>
-          <v-flex md3>
-            <v-btn outline block class="info">1</v-btn>
+        <v-flex md4 pa-1>
+            <v-card height="400px" style="overflow-y: auto;" >
+              <v-img src="https://dummyimage.com/300x200/000/fff" height="200px"></v-img>
+              <v-card-title primary-title>Card 1</v-card-title>
+              <v-card-text>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae minus, enim, ipsam tempora ipsa dignissimos quo eligendi eaque fuga voluptate maxime necessitatibus, soluta vitae. Eligendi voluptas ullam vitae cupiditate reiciendis? 
+              </v-card-text>
+              <v-card-actions>
+                <v-layout row  align-end justify-end>                
+                  
+                  <v-btn color="success">Sucess</v-btn>
+                  <v-btn color="error">Error</v-btn>  
+                
+                </v-layout>
+
+              </v-card-actions>
+              
+            </v-card>
           </v-flex>
-          <v-flex md3>
-            <v-btn outline block class="info">2</v-btn>
-          </v-flex>
+
         </v-layout>
     </v-container>
   </div>
